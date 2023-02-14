a4fbf33218c61cd6aa928cca6709d0f60e328c38
亮玉 韦 <weily@yingmi.cn>
2023-02-14 11:03:58 +0800

    openapi

--- src/main/java/run/antleg/sharp/modules/user/command/CreateUserCommand.java
+++ src/main/java/run/antleg/sharp/modules/user/command/CreateUserCommand.java
@@ -1,11 +1,13 @@
 package run.antleg.sharp.modules.user.command;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateUserCommand {
+    @Schema(defaultValue = "用户的昵称", example = "武沛")
     @NotNull(message = "Parameter displayName is required.")
     @Size(max = 40, message = "Parameter displayName cannot contain more than 40 characters.")
     private String displayName;
--- src/main/java/run/antleg/sharp/routes/UserController.java
+++ src/main/java/run/antleg/sharp/routes/UserController.java
@@ -1,23 +1,32 @@
-package run.antleg.sharp.modules.user;
+package run.antleg.sharp.routes;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import run.antleg.sharp.modules.user.UserHandler;
 import run.antleg.sharp.modules.user.command.CreateUserCommand;
 import run.antleg.sharp.modules.user.model.User;
 
+import java.util.Objects;
+
+@Tag(name = "用户信息")
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
 
-    @Autowired
-    private UserHandler handler;
-
-    @PostMapping("/create-user")
+    @Operation(summary = "创建用户")
+    @PostMapping
     public User createUser(@RequestBody @Valid CreateUserCommand cmd) {
         return handler.createUser(cmd);
+    }
+
+    private final UserHandler handler;
+
+    public UserController(UserHandler handler) {
+        this.handler = Objects.requireNonNull(handler);
     }
 }
