29ac4c14649c13845e06f6155b7384b080a3a361
亮玉 韦 <weily@yingmi.cn>
2023-02-16 09:10:58 +0800

    handle todos

--- src/main/java/run/antleg/sharp/routes/AnthologyRouter.java
+++ src/main/java/run/antleg/sharp/routes/AnthologyRouter.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.ServletException;
 import org.springdoc.core.annotations.RouterOperation;
 import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +13,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.*;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.function.HandlerFunction;
-import org.springframework.web.servlet.function.RequestPredicate;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerResponse;
+import org.springframework.web.servlet.function.*;
 import run.antleg.sharp.modules.anthology.AnthologyHandlers;
 import run.antleg.sharp.modules.anthology.command.CreateAnthologyCommand;
 import run.antleg.sharp.modules.anthology.model.Anthology;
+
+import java.io.IOException;
 
 import static org.springframework.web.servlet.function.RouterFunctions.*;
 import static org.springframework.web.servlet.function.RequestPredicates.*;
@@ -30,10 +29,9 @@ public class AnthologyRouter {
 
     private static final RequestPredicate ACCEPT_JSON = accept(APPLICATION_JSON);
 
-    @Tag(name = "文集") // TODO: not working
     @RouterOperations({
             @RouterOperation(path = "/api/anthologies", method = RequestMethod.POST, consumes = "application/json", operation = @Operation(
-                    operationId = "create-anthology", summary = "创建文集",
+                    operationId = "create-anthology", summary = "创建文集", tags = "文集",
                     requestBody = @RequestBody(content = @Content(schema = @Schema(implementation = CreateAnthologyCommand.class))),
                     responses = {@ApiResponse(responseCode = "200", description = "创建成功", content = @Content(schema = @Schema(implementation = Anthology.class)))}
             ))
@@ -52,15 +50,20 @@ public class AnthologyRouter {
 
     private HandlerFunction<ServerResponse> createAnthology(AnthologyHandlers anthologyHandlers) {
         return (request) -> {
-            var cmd = request.body(CreateAnthologyCommand.class);
-            var br = new DirectFieldBindingResult(cmd, cmd.getClass().getCanonicalName());
-            validator.validate(cmd, br);
-            if (br.hasErrors()) {
-                throw new BindException(br); // TODO: 太复杂了
-            }
+            var cmd = getBody(request, CreateAnthologyCommand.class);
             var anthology = anthologyHandlers.create(cmd);
             return ServerResponse.ok().contentType(APPLICATION_JSON).body(anthology);
         };
     }
 
+
+    private <T> T getBody(ServerRequest request, Class<T> tClass) throws ServletException, IOException, BindException {
+        var cmd = request.body(tClass);
+        var br = new DirectFieldBindingResult(cmd, cmd.getClass().getCanonicalName());
+        validator.validate(cmd, br);
+        if (br.hasErrors()) {
+            throw new BindException(br);
+        }
+        return cmd;
+    }
 }
