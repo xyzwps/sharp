3f2427a3321e7bddcbf1f79423554df1e2e6fc1e
亮玉 韦 <weily@yingmi.cn>
2023-02-22 10:48:12 +0800

    test report

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     groovy
 	id("org.springframework.boot") version "3.0.2"
 	id("io.spring.dependency-management") version "1.1.0"
+    id("test-report-aggregation")
 }
 
 group = "run.antleg"
